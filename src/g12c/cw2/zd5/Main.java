1d76d5d0f35cdc69ebe2f0a3a54cbd1b97085fd7
Contini00 <wojciechowskiwojciech00@gmail.com>
2022-04-10 12:47:59 +0200

    'zadanie5'

--- src/g12c/cw2/zd5/Main.java
+++ src/g12c/cw2/zd5/Main.java
@@ -1,11 +1,41 @@
 package g12c.cw2.zd5;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
 
+/*
+        Adress address1 = new Adress("Poland", "Warszawa", "Koszykowa", 3, 309);
+
+        Person person1 = new Person("Zbigniew", "Krzesło", LocalDate.of(1989, Month.DECEMBER, 13), address1);
+
+        Book book1 = new Book(1L, "Wiedzmin", Genre.Adventure, Lang.Polish, LocalDate.of(1980, Month.SEPTEMBER,30), Collections.singletonList(person1));
+        person1.publishBook(2L, "Pan Kleps", Genre.Detective, Lang.English, LocalDate.of(1975, Month.MAY, 12));
+
+        System.out.println(Book.getBooksList());
+
+        System.out.println("------------");
+
+        book1.borrowBook(person1);
+
+        System.out.println(Book.getBooksList());
+
+        System.out.println("------------");
+
+        book1.returnBook();
+        System.out.println(Book.getBooksList());
+
+        System.out.println("-------------");
+
+        book1.borrowBook(person1);
+
+        System.out.println(Book.getBooksList());
+
+     */
     }
 
 }
--- src/g12c/cw2/zd5/Person.java
+++ src/g12c/cw2/zd5/Person.java
@@ -1,20 +1,38 @@
 package g12c.cw2.zd5;
 
+
+
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Person {
 
     private String name;
     private String surname;
     private LocalDate dateOfBirth;
+    private Adress address;
+    private long age;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", address=" + address +
+                ", age=" + age +
+                '}';
+    }
 
     public String getName(){
         return name;
     }
-    public void setName(String name) throws IllegalAccessException {
+    public void setName(String name){
         if(name == null){
 
-            throw new IllegalAccessException ("Name can't be empty");
+            throw new RuntimeException("Name can't be empty");
         }
         this.name = name;
     }
@@ -23,22 +41,33 @@ public class Person {
     public String getSurname(){
         return surname;
     }
-    public void setSurname(String surname) throws IllegalAccessException {
+    public void setSurname(String surname)  {
         if(surname == null){
 
-            throw new IllegalAccessException ("Surname can't be empty");
+            throw new RuntimeException ("Surname can't be empty");
         }
         this.surname = surname;
     }
 
-    public LocalDate getDateOfBirth() throws Exception {
+    public LocalDate getDateOfBirth() {
         if(dateOfBirth == null){
-            throw new Exception("Can't be empty");
+            throw new RuntimeException("Can't be empty");
         }
         return getDateOfBirth();
     }
-    public void setDateOfBirth(LocalDate dateOfBirth){
+
+    public Person(String name, String surname, LocalDate dateOfBirth, Adress address) {
+        this.name = name;
+        this.surname = surname;
         this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.age = ChronoUnit.YEARS.between(this.dateOfBirth, LocalDate.now());
+    }
+
+    public Book publishBook(Long id, String name, Genre genre, Lang language, LocalDate publishDate) {
+        List<Person> authors = new ArrayList<>();
+        authors.add(this);
+        return new Book(id, name, genre, language, publishDate, authors);
     }
 
 
